c6e56c35da0b8c2a09f334fa10c2be39900efca5
Kyle Maxwell <kyle@kylemaxwell.com>
2009-10-12 15:21:55 -0700

    rework client

--- lib/websolr-rails.rb
+++ lib/websolr-rails.rb
@@ -2,31 +2,53 @@
 # gem is loaded and WEBSOLR_URL is defined.
 
 if ENV["WEBSOLR_URL"]
+  
+  def websolr_install_sunspot
+    Sunspot.config.solr.url = ENV["WEBSOLR_URL"]
+  end
+    
+  def websolr_install_acts_as_solr
+    eval <<-RUBY
+      module ::ActsAsSolr
+        class Post        
+            def self.execute(request)
+              begin
+                connection = Solr::Connection.new(ENV["WEBSOLR_URL"])
+                return connection.send(request)
+              rescue 
+                raise "Couldn't connect to the Solr server at #{url}. #{$!}"
+                false
+              end
+            end
+          end
+        end
+      end
+    RUBY
+  end   
+  
+  begin
+    ActsAsSolr
+    websolr_install_acts_as_solr
+  rescue LoadError
+  end
+
+  begin
+    Sunspot
+    websolr_install_acts_as_solr
+  rescue LoadError
+  end
+  
   module Kernel
     alias_method :require_without_solr_hooks, :require
     def require(string)
       require_without_solr_hooks(string)
       case string
       when "acts_as_solr"
-        eval <<-RUBY
-          module ::ActsAsSolr
-            class Post        
-                def self.execute(request)
-                  begin
-                    connection = Solr::Connection.new(ENV["WEBSOLR_URL"])
-                    return connection.send(request)
-                  rescue 
-                    raise "Couldn't connect to the Solr server at #{url}. #{$!}"
-                    false
-                  end
-                end
-              end
-            end
-          end
-        RUBY
+        websolr_install_acts_as_solr
       when "sunspot"
-        Sunspot.config.solr.url = ENV["WEBSOLR_URL"]
+        websolr_install_sunspot
       end
     end
   end
+  
 end
