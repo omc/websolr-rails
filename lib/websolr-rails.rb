367166f984645d92257002981e18b8ceb3393664
Kyle Maxwell <kyle@kylemaxwell.com>
2009-10-20 14:08:03 -0700

    recover sunspot errors

--- lib/websolr-rails.rb
+++ lib/websolr-rails.rb
@@ -6,6 +6,8 @@ if ENV["WEBSOLR_URL"]
   
   begin
     require "sunspot/rails/configuration"
+    require "sunspot/rails/searchable"
+    require "sunspot/rails/request_lifecycle"
     module Sunspot #:nodoc:
       module Rails #:nodoc:
         class Configuration
@@ -21,6 +23,60 @@ if ENV["WEBSOLR_URL"]
         end
       end
     end
+    
+    module Sunspot #:nodoc:
+      module Rails #:nodoc:
+        # 
+        # This module adds an after_filter to ActionController::Base that commits
+        # the Sunspot session if any documents have been added, changed, or removed
+        # in the course of the request.
+        #
+        module RequestLifecycle
+          class <<self
+            def included(base) #:nodoc:
+              base.after_filter do
+                begin
+                  if Sunspot::Rails.configuration.auto_commit_after_request?
+                    Sunspot::Rails.master_session.commit_if_dirty
+                  elsif Sunspot::Rails.configuration.auto_commit_after_delete_request?
+                    Sunspot::Rails.master_session.commit_if_delete_dirty
+                  end
+                rescue Exception => e
+                  ActionController::Base.logger.error e.message
+                  ActionController::Base.logger.error e.backtrace.join("\n")
+                  false
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    
+    
+    module Sunspot
+      module Rails 
+        module Searchable
+          module InstanceMethods
+            %w[index index! remove_from_index remove_from_index!].each do |method|
+              new_name = method =~ /!/ ? method.gsub("!", "") + "_with_caught_errors!" : "#{method}_with_caught_errors"
+              old_name = new_name.sub("_with_", "_without_")
+              define_method(new_name) do
+                begin
+                  send(old_name)
+                rescue Exception => e
+                  logger.error e.message
+                  logger.error e.backtrace.join("\n")
+                  false
+                end
+              end
+              alias_method_chain method, :caught_errors
+            end
+          end
+        end
+      end
+    end
+    
   rescue LoadError
     #ignore
   end
--- websolr-rails.gemspec
+++ websolr-rails.gemspec
@@ -9,7 +9,7 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Kyle Maxwell"]
-  s.date = %q{2009-10-19}
+  s.date = %q{2009-10-20}
   s.default_executable = %q{websolr}
   s.description = %q{Makes Sunspot and acts_as_solr play with WebSolr}
   s.email = %q{kyle@kylemaxwell.com}
